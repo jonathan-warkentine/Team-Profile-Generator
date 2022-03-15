3947151f011d90abea4d60f975df1d830f737a25
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-03-15 02:24:02 -0400

    All tests working

--- _tests_/Team.test.js
+++ _tests_/Team.test.js
@@ -1,5 +1,3 @@
-const { it, expect } = require('@jest/globals');
-const { describe } = require('yargs');
 const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
 const Manager = require('../lib/Manager');
@@ -7,11 +5,8 @@ const Intern = require('../lib/Intern');
 const Team = require('../lib/Team');
 
 
-// const engineer = new Engineer ("Fred", 1, "fred@freddy.com", "fredstergit");
-// const manager = new Manager("Bob", 2, "bob@freddy.com", 4);
-// const intern = new Intern("Marvin", 3, "marvin@google.com", "Harvard");
-
 describe('Class Team', () => {
+    
     // describe('The build() method', () => {
     //     it('')
     // })
@@ -23,7 +18,7 @@ describe('Class Team', () => {
             email: "bob@bob.com",
             github: "bob-github",
             role: "Engineer"
-        }
+        };
         
         it('adds an object of class Engineer, Intern, or Manager (subclass of Class Employee) to the "members" property', () => {
             const team = new Team ();
@@ -33,18 +28,14 @@ describe('Class Team', () => {
             expect(team.members.length).toBe(1);
             expect(team.members[0]).toBeInstanceOf(Employee);
             expect(team.members[0]).toBeInstanceOf(Engineer);
-
-            expect(team.members.length).toBe(0);
-            team.addMember(answers);
-            expect(team.members.length).toBe(1);
-            expect(team.members[0]).toBeInstanceOf(Employee);
-            expect(team.members[0]).toBeInstanceOf(Engineer);
-        })
+        
+        });
 
         it('Does not return anything', () => {
+            const team = new Team ();
             expect(team.addMember(answers)).toBe(undefined);
-        })
-    })
+        });
+    });
 
-    describe('The htmlCreateCard method')
-})
+    // describe('The htmlCreateCard method')
+});
